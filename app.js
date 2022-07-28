a4c5715dc3ca0f552add2177880654262a80f5a0
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-28 11:27:52 +0200

    Refractoring

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-const teamOneResult = document.querySelector("#teamOne");
-const teamTwoResult = document.querySelector("#teamTwo");
-const teamOneBtn = document.querySelector("#team1Btn");
-const teamTwoBtn = document.querySelector("#team2Btn");
+const t1 = {
+  score: 0,
+  button: document.querySelector("#team1Btn"),
+  display: document.querySelector("#teamOne"),
+};
+const t2 = {
+  score: 0,
+  button: document.querySelector("#team2Btn"),
+  display: document.querySelector("#teamTwo"),
+};
+
 const resetBtn = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#playTo");
 
-let teamOneScore = 0;
-let teamTwoScore = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-teamOneBtn.addEventListener("click", function () {
-  //   updatingScore(teamOneScore, teamOneResult);
+function updateScores(team, opponent) {
   if (!isGameOver) {
-    teamOneScore++;
-    if (teamOneScore === winningScore) {
+    team.score += 1;
+    if (team.score === winningScore) {
       isGameOver = true;
-      teamOneResult.classList.add("has-text-success");
-      teamTwoResult.classList.add("has-text-danger");
-      teamOneBtn.disabled = true;
-      teamTwoBtn.disabled = true;
+      team.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
+      team.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    teamOneResult.textContent = teamOneScore;
+    team.display.textContent = team.score;
   }
+}
+
+t1.button.addEventListener("click", function () {
+  updateScores(t1, t2);
 });
-teamTwoBtn.addEventListener("click", function () {
-  //   updatingScore(teamTwoScore, teamTwoResult);
-  if (!isGameOver) {
-    teamTwoScore++;
-    if (teamTwoScore === winningScore) {
-      isGameOver = true;
-      teamTwoResult.classList.add("has-text-success");
-      teamOneResult.classList.add("has-text-danger");
-      teamOneBtn.disabled = true;
-      teamTwoBtn.disabled = true;
-    }
-    teamTwoResult.textContent = teamTwoScore;
-  }
+t2.button.addEventListener("click", function () {
+  updateScores(t2, t1);
 });
 
 winningScoreSelect.addEventListener("change", function () {
@@ -46,22 +43,12 @@ winningScoreSelect.addEventListener("change", function () {
 
 resetBtn.addEventListener("click", reset);
 
-// function updatingScore(teamScore, teamResult) {
-//   if (teamScore !== 5) {
-//     teamScore++;
-//     console.log(teamScore);
-//     teamResult.textContent = teamScore;
-//   }
-// }
-
 function reset() {
-  teamOneScore = 0;
-  teamTwoScore = 0;
-  teamOneResult.textContent = teamOneScore;
-  teamTwoResult.textContent = teamTwoScore;
   isGameOver = false;
-  teamOneResult.classList.remove("has-text-success", "has-text-danger");
-  teamTwoResult.classList.remove("has-text-success", "has-text-danger");
-  teamOneBtn.disabled = false;
-  teamTwoBtn.disabled = false;
+  for (let t of [t1, t2]) {
+    t.score = 0;
+    t.display.textContent = 0;
+    t.display.classList.remove("has-text-success", "has-text-danger");
+    t.button.disabled = false;
+  }
 }
